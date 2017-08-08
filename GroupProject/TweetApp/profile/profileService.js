076d766e81b3b774586d1cf879bd78f502b023ae
OlgaVasy <ovasilyeva22@gmail.com>
2017-08-07 22:28:34 -0500

    updated startPage

--- GroupProject/TweetApp/profile/profileService.js
+++ GroupProject/TweetApp/profile/profileService.js
@@ -2,8 +2,6 @@ angular.module('tweetModule')
 
 .service('profileService', ['$http',function(http) {
 
-  this.getProfile = (id) =>
-    http.get('http://localhost:8080/profile/' + id)
-    .then((resp) => { return resp.data })
+
 
 }])
---
+++ GroupProject/TweetApp/startPage/startPageController.js
@@ -0,0 +1,6 @@
+angular.module('tweetModule')
+
+.controller('startPageController', [function() {
+
+
+}])
