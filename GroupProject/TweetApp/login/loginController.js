aeeeeaa592f69f912866985f961c72c1d2183453
unknown <sfp3@gatech.edu>
2017-08-08 09:13:11 -0500

    please don't screw up 2

--- GroupProject/TweetApp/login/loginController.js
+++ GroupProject/TweetApp/login/loginController.js
@@ -1,4 +1,10 @@
 angular.module('tweetModule')
-.controller('loginController', function() {
+.controller('loginController', ['loginService', function(creds) {
 
-})
+  this.loginService = creds;
+
+  this.credentials = {
+    username: 0,
+    password: 0
+  }
+}])
