8b2d7660ba3aec02f03b9771f1f95ef0fa7b369b
OlgaVasy <ovasilyeva22@gmail.com>
2017-08-07 21:38:13 -0500

    added start and profile pages

--- GroupProject/TweetApp/route.js
+++ GroupProject/TweetApp/route.js
@@ -1,5 +1,21 @@
 angular.module('tweetModule')
 .config(function($stateProvider, $urlRouterProvider) {
 
+  let startPageState = {
+   name: 'start',
+   url: '/',
+   component: 'startPageComponent'
+ }
 
-})
+  let profileState = {
+      name: 'profile',
+      url: '/profile',
+      component: 'profileComponent'  
+
+    }
+    $stateProvider.state(startPageState)
+    $stateProvider.state(profileState)
+
+    $urlRouterProvider.otherwise('/');
+
+  });
