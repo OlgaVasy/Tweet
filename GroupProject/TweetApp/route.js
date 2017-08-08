94ee6df3a2e7a4a5aad991fc158fe1a960ef7a27
Philip Bennett <bennettpa2@gmail.com>
2017-08-07 20:02:40 -0500

    fixed merge issues

--- GroupProject/TweetApp/route.js
+++ GroupProject/TweetApp/route.js
@@ -1,5 +1,13 @@
 angular.module('tweetModule')
 .config(function($stateProvider, $urlRouterProvider) {
 
+let profileState = {
+  name: 'profile',
+  url: '/profile',
+  component: 'profileComponent'
+}
 
-})
+
+ $urlRouterProvider.otherwise('/')
+
+ })
