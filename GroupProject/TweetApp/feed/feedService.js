c1b6095264cca0c518300484d45ffeb628a70a93
AIRossow <andrew.i.rossow@gmail.com>
2017-08-08 18:30:33 -0500

    Altered the layout of the tweets in the feed just to see what y'all thought

--- GroupProject/TweetApp/feed/feedService.js
+++ GroupProject/TweetApp/feed/feedService.js
@@ -4,12 +4,10 @@ angular.module('tweetModule')
     this.user = username;
   return http.get('http://localhost:8080/user/users/@'+username+'/feed').then(
     (failure) => {
-      this.feed = failure.data
       console.log('Could not load feed')
     },
     (success) => {
       this.feed = success.data
-      console.log(this.feed)
     }
     )
   }
