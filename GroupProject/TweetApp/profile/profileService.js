dc78e971e8f8b5594eb4031d9fcfd6005456ec5a
AIRossow <andrew.i.rossow@gmail.com>
2017-08-09 18:21:05 -0500

    Changed a few things, can now go to another users profile and follow them/unfollow them, logged in name changes but follower is applied to the original signed in user

--- GroupProject/TweetApp/profile/profileService.js
+++ GroupProject/TweetApp/profile/profileService.js
@@ -3,8 +3,9 @@ angular.module('tweetModule')
 .service('profileService', ['$http',function(http) {
 
   this.getProfile = (username) => {
+    this.followName = username
     username = sessionStorage.getItem('username')
-    http.get('http://localhost:8080/user/users/@'+username).then(
+    http.get('http://localhost:8080/user/users/@'+this.followName).then(
       (successResponse)=> {
         this.userProfile = successResponse.data
       },
@@ -14,4 +15,41 @@ angular.module('tweetModule')
 
    );
   }
+  this.count = 1
+  this.follow = () => {
+    this.test1 = sessionStorage.getItem('password').toString()
+    this.test2 = sessionStorage.getItem('username').toString()
+    let credentials = {credentials: {password: this.test1,
+                                      username:this.test2 }}
+
+    if (this.count%2 != 0) {
+      return http({
+        method: 'POST',
+        url: 'http://localhost:8080/user/users/@'+this.followName+'/follow',
+        data: credentials})
+      .then((failure) => {
+        console.log(failure.data)
+      },
+      (success) => {
+        console.log('Following ' + this.followName)
+        $('followUnfollow').html('Unfollow')
+        this.count++
+      }
+      )
+    } else {
+      return http({
+        method: 'POST',
+        url: 'http://localhost:8080/user/users/@' + this.followName + '/unfollow',
+        data: credentials})
+        .then((failure) => {
+          console.log(failure.data)
+        },
+        (success) => {
+          console.log('Unfollowed ' + this.followName)
+          this.count++
+        }
+        )
+    }
+  }
+
   }])
--- GroupProject/TweetApp/profile/profileSubpage/profileSubpageService.js
+++ GroupProject/TweetApp/profile/profileSubpage/profileSubpageService.js
@@ -3,7 +3,7 @@ angular.module('tweetModule')
 .service('profileSubpageService', ['$http',function(http) {
 
   this.getUserAllTweets = (username) => {
-    username = sessionStorage.getItem('username')
+    //username = sessionStorage.getItem('username')
     http.get('http://localhost:8080/user/users/@'+username+'/tweets').then(
       (successResponse)=> {
         this.userTweets = successResponse.data
