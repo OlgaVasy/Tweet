18e79005967f2c2aafbf1e482ae651a2a7a4e50f
OlgaVasy <ovasilyeva22@gmail.com>
2017-08-10 14:33:44 -0500

    added link to start page from profile

--- GroupProject/TweetApp/startPage/startPageController.js
+++ GroupProject/TweetApp/startPage/startPageController.js
@@ -1,11 +1,4 @@
 angular.module('tweetModule')
-<<<<<<< HEAD
-
-.controller('startPageController', ['startPageService','profileService', 'loginService', function(startService, profileService, loginService) {
-  this.startPageService = startService
-  this.profileService = profileService
-  this.loginSevice = loginService
-=======
 .controller('startPageController', ['startPageService', 'profileService', 'tweetService', function(startService, profileService, tweetService) {
   this.startPageService = startService
   this.profileService = profileService
@@ -18,6 +11,5 @@ angular.module('tweetModule')
       username: ''
     }
   }
->>>>>>> 02a681539e8117e0e748a0cbba8214cf7411356b
 
 }])
