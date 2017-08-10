2772270b7b9036ebdca68c6a84353e69767f3941
Philip Bennett <bennettpa2@gmail.com>
2017-08-10 11:02:19 -0500

    issues with merge fixed

--- GroupProject/TweetApp/profile/profileController.js
+++ GroupProject/TweetApp/profile/profileController.js
@@ -1,13 +1,7 @@
 angular.module('tweetModule')
 
-<<<<<<< HEAD
-.controller('profileController', ['profileService', 'tweetService', function(manageProfile, tweetService) {
-  this.profileService = manageProfile
-  this.tweetService = tweetService
-=======
 .controller('profileController', ['profileService','tweetService', function(manageProfile, manageTweet) {
   this.profileService = manageProfile
   this.tweetService = manageTweet
 
->>>>>>> 8e7e3b53329fbc45a3137b121184aaf96ad994d8
 }])
