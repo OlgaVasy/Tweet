0517059bc3f28d5d46acf1aa6b3d30e0af4edbdd
AIRossow <andrew.i.rossow@gmail.com>
2017-08-10 21:20:38 -0500

    did some formatting in the profile service

--- GroupProject/TweetApp/profile/profileService.js
+++ GroupProject/TweetApp/profile/profileService.js
@@ -1,41 +1,41 @@
 angular.module('tweetModule')
 
-.service('profileService', ['$http','$state',function(http,$state) {
+.service('profileService', ['$http', '$state', function (http, $state) {
   this.followedOrNah = 'Follow'
   this.likedTweetsButton = 'Liked Tweets'
 
   this.getProfile = (username) => {
     this.followName = username
     this.username = sessionStorage.getItem('username')
-    http.get('http://localhost:8080/user/users/@'+this.followName).then(
-      (successResponse)=> {
+    http.get('http://localhost:8080/user/users/@' + this.followName).then(
+      (successResponse) => {
         this.userProfile = successResponse.data
       },
       (failureResponse) => {
-      (this.userProfile=failureResponse.data)
+        (this.userProfile = failureResponse.data)
       }
 
-   );
+   )
   }
   this.logOut = () => {
-      sessionStorage.clear()
-      $state.go('start')
+    sessionStorage.clear()
+    $state.go('start')
   }
   this.goToMyProfile = () => {
-      this.username = sessionStorage.getItem('username')
-      $state.go('profile', {username: this.username})
+    this.username = sessionStorage.getItem('username')
+    $state.go('profile', {username: this.username})
   }
 
   this.follow = () => {
     this.test1 = sessionStorage.getItem('password').toString()
     this.test2 = sessionStorage.getItem('username').toString()
     let credentials = {credentials: {password: this.test1,
-                                      username:this.test2 }}
+                                     username: this.test2 }}
 
     if (this.followedOrNah === 'Follow') {
       return http({
         method: 'POST',
-        url: 'http://localhost:8080/user/users/@'+this.followName+'/follow',
+        url: 'http://localhost:8080/user/users/@' + this.followName + '/follow',
         data: credentials})
       .then((failure) => {
         console.log(failure.data)
@@ -66,69 +66,66 @@ angular.module('tweetModule')
   this.checkFollowing = () => {
     this.likedTweetsButton = 'Liked Tweets'
     this.shouldShow = true
-     http.get('http://localhost:8080/user/users/@'+ this.username+'/following')
+    http.get('http://localhost:8080/user/users/@' + this.username + '/following')
      .then(
-       (failure)=> {
+       (failure) => {
          console.log('failure')
        },
        (success) => {
          success.data.forEach((item) => {
-           if(item.username === this.followName) {
+           if (item.username === this.followName) {
              this.followedOrNah = 'Unfollow'
            }
          })
        }
-    );
+    )
+  }
 
- }
-
- this.getLikes = () =>  {
-   return http.get('http://localhost:8080/user/users/@'+this.followName+'/likedTweets')
+  this.getLikes = () => {
+    return http.get('http://localhost:8080/user/users/@' + this.followName + '/likedTweets')
    .then(
      (failure) => {
      },
      (success) => {
        this.shouldShow = !this.shouldShow
-       if(!this.shouldShow) {
-          this.likedTweetsButton = 'User Tweets'
-        } else {
-          this.likedTweetsButton = 'Liked Tweets'
-        }
+       if (!this.shouldShow) {
+         this.likedTweetsButton = 'User Tweets'
+       } else {
+         this.likedTweetsButton = 'Liked Tweets'
+       }
        this.likedTweets = success.data
      }
    )
- }
-
+  }
 
   this.getUserAllTweets = (username) => {
-    http.get('http://localhost:8080/user/users/@'+username+'/tweets').then(
-      (succesResponse)=> {
-      this.userTweets=successResponse.data
-
+    http.get('http://localhost:8080/user/users/@' + username + '/tweets').then(
+      (successResponse) => {
+        this.userTweets = successResponse.data
       },
       (failureResponse) => {
-      this.userTweets=failureResponse.data
+        this.userTweets = failureResponse.data
       }
-   );
+   )
   }
   this.getFollowing = (username) => {
-    http.get('http://localhost:8080/user/users/@'+username+'/following').then(
-      (successResponse)=> {
+    http.get('http://localhost:8080/user/users/@' + username + '/following').then(
+      (successResponse) => {
         this.userFollowing = successResponse.data
       },
       (failureResponse) => {
-      (this.userFollowing=failureResponse.data)
+        (this.userFollowing = failureResponse.data)
       }
-   );
+   )
   }
   this.getFollowers = (username) => {
-    http.get('http://localhost:8080/user/users/@'+username+'/followers').then(
-      (successResponse)=> {
+    http.get('http://localhost:8080/user/users/@' + username + '/followers').then(
+      (successResponse) => {
         this.userFollowers = successResponse.data
       },
       (failureResponse) => {
-      (this.userFollowers=failureResponse.data)
+        (this.userFollowers = failureResponse.data)
       }
-   );
+   )
   }
-  }])
+}])
